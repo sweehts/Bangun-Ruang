df3fad875fc431df723274aa188c53a9b957a4ed
sweehts <156729206+sweehts@users.noreply.github.com>
2024-01-30 09:41:41 +0700

    Update LimasSegitiga.java

--- ruang/LimasSegitiga.java
+++ ruang/LimasSegitiga.java
@@ -10,12 +10,12 @@ public class LimasSegitiga extends BangunRuang{
     
     @Override
     double volume(){
-    double volume = (1/2 * alas * tS) * tL * 1/6;
+    double volume = (1/2 * alas * tS) * tL * 1/3;
     System.out.println("volume Limas segitiga : " + volume);
     return volume;
 }
     double luasPermukaan(double lp){
-       double luasPermukaan = 1/3 * (1/2 * alas * tS) * tL;
+       double luasPermukaan = 1/2 * (1/2 * alas * tS) * tL;
         System.out.println("luas Limas segitiga : " + lp);
         return luasPermukaan;
     }
